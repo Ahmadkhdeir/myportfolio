82daaa25e1ede2e5eee5c411d3d878d4f0bed001
Ahmad Khdeir <khdeir@stud.uni-frankfurt.de>
2024-11-28 17:21:44 +0100

    first commit

---
+++ scripts/projects.js
@@ -0,0 +1,13 @@
+var PROJECTS = [
+    {
+        img: "project1.webp",
+        title: "project1",
+        description: "a view on pro1"
+    },
+    {
+        img: "project2.webp",
+        title: "project2",
+        description: "a view on pro2"
+    }
+]
+
